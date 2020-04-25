f46f15bd735fca60996bcc5b08a405e2a0d0f5a9
Henri Hulski <henri.hulski@gazeta.pl>
2020-04-25 22:53:27 +0200

    Cleanup after black formatting

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     install_requires=["morepath >= 0.19", "cerberus >= 1.3.2, < 2.0.0"],
     extras_require=dict(
         test=["pytest >= 2.9.1", "pytest-remove-stale-bytecode", "webtest"],
-        coverage=["pytest-cov",],
-        pep8=["flake8", "black",],
+        coverage=["pytest-cov"],
+        pep8=["flake8", "black"],
     ),
 )
