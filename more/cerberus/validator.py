7b5e0e12f3c439de96795604c51222172d94eb9f
Henri Hulski <henri.hulski@gazeta.pl>
2020-04-25 22:03:46 +0200

    Format code base with Black

--- more/cerberus/validator.py
+++ more/cerberus/validator.py
@@ -3,6 +3,6 @@ from cerberus import Validator
 
 class CerberusValidator(Validator):
     def __init__(self, *args, **kwargs):
-        if 'request' in kwargs:
-            self.request = kwargs['request']
+        if "request" in kwargs:
+            self.request = kwargs["request"]
         super().__init__(*args, **kwargs)
