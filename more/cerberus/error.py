48f6434bcbe04302177d41288c35811f6db4ce1a
Henri Hulski <henri.hulski@gazeta.pl>
2020-04-25 18:25:16 +0200

    Remove support for Python 2 and upgrade Python 3

--- more/cerberus/error.py
+++ more/cerberus/error.py
@@ -1,4 +1,4 @@
 class ValidationError(Exception):
     def __init__(self, errors):
-        super(ValidationError, self).__init__("Cerberus validation error")
+        super().__init__("Cerberus validation error")
         self.errors = errors
