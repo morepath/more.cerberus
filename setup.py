ed4217c2e925abd2c167c691c3f9bc952940813e
Henri Hulski <henri.hulski@gazeta.pl>
2020-04-26 14:35:01 +0200

    Back to development: 0.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (
 
 setup(
     name="more.cerberus",
-    version="0.3",
+    version="0.4.dev0",
     description="Cerberus validation support for Morepath",
     long_description=long_description,
     author="Henri Hulski",
